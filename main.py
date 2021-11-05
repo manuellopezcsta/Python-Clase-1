d4a2c0137c6eae19124baebec3dbd2d3cf423170
Lopez Manuel <manuel.lopez.csta@gmail.com>
2021-11-05 20:33:22 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+y = 123
+a = 10
+b = 5
+
+y = "hola mundo"
+
+# Esto es Python
+def sumar(a, b):
+    # Como poner la def de la funcion.
+    """ Sumar dos numeros a y b""" 
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+super_lista = [1,2,4,5,6,7,8,9,10]
+
+z = sumar(a,b)
+print(3 in super_lista) # False
+print(4 in super_lista) # True
+
+print(z)
+
+count = 0
+while count < 3:
+    print(count)
+    count = int(input("Ingrese un num"))
+print("Fin del programa")
+
+
+
+
